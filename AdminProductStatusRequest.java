f51d7ee30c690f8f6493e2ac2ed2e5fc00c8512a
Mohamed Irfan M <rhtmmirfan@gmail.com>
2026-09-20 10:44:12 +0530

    Add AdminProductStatusRequest class
    Add AdminProductStatusRequest class

---
+++ AdminProductStatusRequest.java
@@ -0,0 +1,31 @@
+package com.blackmart.blackmart.dto;
+
+public class AdminProductStatusRequest {
+
+    private Long productId;
+    private String status;
+
+    public AdminProductStatusRequest() {
+    }
+
+    public AdminProductStatusRequest(Long productId, String status) {
+        this.productId = productId;
+        this.status = status;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
