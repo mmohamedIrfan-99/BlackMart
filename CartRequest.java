2d2b3f0aba6496dd22d27d2e121f30bc53feb7fd
Mohamed Irfan M <rhtmmirfan@gmail.com>
2026-09-20 10:31:48 +0530

    Create CartRequest class for cart operations
    Create CartRequest class for cart operations

---
+++ CartRequest.java
@@ -0,0 +1,31 @@
+package com.blackmart.blackmart.dto;
+
+public class CartRequest {
+
+    private Long productId;
+    private int quantity;
+
+    public CartRequest() {
+    }
+
+    public CartRequest(Long productId, int quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
